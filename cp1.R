bc93f42df34f19f7b6fc381a4894a8ee6a8e3724
Arakcheyev Alex <arakcheyev.alex@gmail.com>
2016-03-05 19:08:23 +0500

    Creating report

--- cp1.R
+++ cp1.R
@@ -5,20 +5,50 @@ set.seed(9988)
 train_path <- "pml-training.csv"
 test_path <- "pml-testing.csv"
 
-training <- read.csv(train_path, na.strings = c("NA", ""))
-testing <- read.csv(test_path, na.strings = c("NA", ""))
+f_train <- read.csv(train_path, na.strings = c("NA", ""))
+f_test <- read.csv(test_path, na.strings = c("NA", ""))
 
-inTrain <- createDataPartition(y = training$classe, p = 0.6, list = FALSE)
-training_data <- training[inTrain, ]
-testing_data <- training[-inTrain, ]
-dim(training_data)
-dim(testing_data)
+inTrain <- createDataPartition(y = f_train$classe, p = 0.75, list = FALSE)
+p_train <- f_train[inTrain, ]
+p_test <- f_train[-inTrain, ]
+dim(p_train)
+dim(p_test)
 
-nzv <- nearZeroVar(training_data, saveMetrics = TRUE)
+nzv <- nearZeroVar(p_train)
+p_train <- p_train[, -nzv]
+p_test <- p_test[, -nzv]
 
-training_data <- training_data[, -nzv]
-testing_data <- testing_data[, -nzv]
+NA_mark <- sapply(p_train, function(x) mean(is.na(x))) > 0.95
+p_train <- p_train[, NA_mark == FALSE]
+p_test <- p_test[, NA_mark == FALSE]
 
+p_train <- p_train[, -(1:5)]
+p_test <- p_test[, -(1:5)]
 
+fitControl <- trainControl(method = "cv", number = 3, verboseIter = FALSE)
+modRF1 <- train(classe ~ ., data = p_train, method = "rf", trControl = fitControl)
+modRF1$finalModel
 
+modRF1$xlevels
 
+predictions <- predict(modRF1, p_test)
+confusionMatrix(p_test$classe, predictions)
+
+## real test
+nzv <- nearZeroVar(training)
+f_train <- f_train[, -nzv]
+f_test <- f_test[, -nzv]
+dim(f_train)
+dim(f_test)
+
+NA_mark <- sapply(f_train, function(x) mean(is.na(x))) > 0.95
+f_train <- f_train[, NA_mark == FALSE]
+f_test <- f_test[, NA_mark == FALSE]
+f_train <- f_train[, -(1:5)]
+f_test <- f_test[, -(1:5)]
+
+modRF2 <- train(classe ~ ., data = f_train, method = "rf", trControl = fitControl)
+modRF2$finalModel
+predictions <- predict(modRF2, f_test)
+
+predictions
